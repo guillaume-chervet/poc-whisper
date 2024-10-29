18693a086a9d6f5859b641760f01d2ae1296584b
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-10-29 17:33:45 +0100

    feat(deploy): test deployment to kubernetes azure

--- production/webapp/src/App.jsx
+++ production/webapp/src/App.jsx
@@ -2,11 +2,21 @@ import './App.css'
 import AudioRecorderComponent from "./AudioRecorderComponent.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <AudioRecorderComponent />
-    </div>
-  );
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+       return (
+        <div className="App">
+          <AudioRecorderComponent />
+        </div>
+      );
+    } else {
+        console.error("L'API mediaDevices n'est pas supportée par ce navigateur.");
+        return (
+            <div className="App">
+              <p>L'API mediaDevices n'est pas supportée par ce navigateur.</p>
+            </div>
+          );
+    }
+
 }
 
 export default App
